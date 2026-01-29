8e4225d56767813cf431d6989628ea8484bc1ecc
Uppala Anand <uppalaanand410@gmail.com>
2026-01-29 19:33:45 +0530

    onlineLearnPlatform assignment completed

--- assignments/session2/onlineLearnPlatform/app.js
+++ assignments/session2/onlineLearnPlatform/app.js
@@ -33,7 +33,7 @@ const roles = {
 //   -> Get only active users
 let activeUsers = users.filter(user => user.active === true);
 //   -> Extract names of active users
-let { names } = activeUsers;
+let names = activeUsers.map(m => m.name);
 //   -> Check if any admin exists
 let admin = users.find(user => user.role === 'admin');
 //   -> Find user by id
@@ -47,18 +47,45 @@ let pubCourses = courses.filter(course => course.published === true);
 //   -> Sort courses by price (high → low)
 
 //   -> Extract { title, price } only
+const { title, price } = courses;
 //   -> Calculate total value of published courses
+const total = pubCourses.reduce((acc, val) => acc+val.price, 0);
 //   -> Add a new course immutably
+courses.push({ id: 104, title: "Java", price: 888, published: true })
 
 // MODULE 3: SHOPPING CART ENGINE 
 //   -> Merge cart with courses to get full course info
+const detailedCart = cart.map(item => {
+  const course = courses.find(c => c.id === item.courseId);
+  return {
+    ...course,
+    qty: item.qty,
+    totalPrice: course.price * item.qty
+  };
+});
+console.log(detailedCart)
 //   -> Calculate total cart amount
+const totalCart = detailedCart.reduce((acc, val) => acc+val.totalPrice, 0);
 //   -> Increase quantity of a course (immutably)
+const increaseQty = cart.map(item => item.courseId === 101
+  ? { ...item, qty : item.qty + 1} : item
+);
 //   -> Remove a course from cart
+const removeCart = cart.filter(item => item.courseId != 103);
 //   -> Check if all cart items are paid courses
+
 
 // MODULE 4: ROLE & PERMISSION ENGINE
 //   -> Get all role names
+const roleNames = Object.keys(roles);
 //   -> Check if student can delete
+const canStudentDelete = roles.student.includes("delete");
 //   -> Create a flat list of all unique permissions
+const allPermissions = [
+  ...new Set(Object.values(roles).flat())
+];
 //   -> Add new role moderator immutably
+const updatedRoles = {
+  ...roles,
+  moderator: ["view", "update"]
+};
